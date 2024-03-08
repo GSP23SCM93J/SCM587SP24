eb735ea6e054ed68428fa5eddc115a9b72019905
SPM587SP24 <npatel157@hawk.iit.edu>
2024-03-07 23:39:50 -0600

    Second Commit

--- o.java
+++
@@ -1,5 +0,0 @@
-public class O {
-	public static void main(String[] args) {
-		System.out.println("O");
-	}
-}
--- v.java
+++
@@ -1,5 +0,0 @@
-public class V {
-	public static void main(String[] args) {
-		System.out.println("V");
-	}
-}
